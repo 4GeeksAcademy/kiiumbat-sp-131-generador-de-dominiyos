eb2067e918a56b1c7ecd5aa03dcdc6faa0c99cb6
Marc Caritg <k.kiumbat@gmail.com>
2026-01-31 11:26:04 +0000

    first comit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronom = ["el" , "Ella" , "nosotros" , "vosotros"] 
+  let adj = ["guapo" , "feo" , "tolerante" , "alegre"] 
+  let sustantivo = ["cerdo" , "caserio" , "avion" , "boa"] 
+
+for (let p of pronom){
+  for (let a of adj){
+    for (let s of sustantivo){
+      console.log (p+a+s+".com")
+    }
+  }
+}
 };
